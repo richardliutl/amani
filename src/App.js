4b97677f7db80c37e16e8f67d4c389da14f65fa8
Shenal S Kotuwewatta <shenal@mit.edu>
2021-09-18 15:49:44 -0400

    create navbar layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <NavBar/>
+      <header className="App-body">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Amani!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
