f34ca8d1a503283c9076be735c4ee32e332d33d4
limarta <limarta@mit.edu>
2021-09-18 16:47:26 -0400

    Added requirement, API key, fixed example. Added python to .gitignore

--- api/api_example.py
+++ api/api_example.py
@@ -4,15 +4,13 @@ from datetime import datetime
 
 gmaps = googlemaps.Client(key= api_key)
 
-# Geocoding an address
 geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+print(geocode_result)
 
+results = gmaps.places(query="indian restaurants", radius=1000)
+print(results)
 # Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+# now = datetime.now()
