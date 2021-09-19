471f4994e46eae65fdc88abbf9e818f9b77c0947
richardliutl <richardliutl@gmail.com>
2021-09-18 23:47:50 -0400

    Adds firebase SDK

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgi3ec71yWT0bDupqkpDt7jGT69Fgj_Bk",
+  authDomain: "amani-web.firebaseapp.com",
+  projectId: "amani-web",
+  storageBucket: "amani-web.appspot.com",
+  messagingSenderId: "614058163380",
+  appId: "1:614058163380:web:41eab81a19795946146be1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
