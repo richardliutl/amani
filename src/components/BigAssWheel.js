53c15b7912e0a7f8d9556ff3fbf34835dcecbf4e
Shenal S Kotuwewatta <shenal@mit.edu>
2021-09-18 20:48:01 -0400

    added restaurant card to big ass wheel

--- src/components/BigAssWheel.js
+++ src/components/BigAssWheel.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from 'react';
 import * as d3 from "d3";
 import * as d3ScaleChromatic from "d3-scale-chromatic";
+
+import RestaurantCard from "./RestaurantCard";
 
 export const useD3 = (renderChartFn, dependencies) => {
     const ref = React.useRef();
@@ -15,8 +17,9 @@ export const useD3 = (renderChartFn, dependencies) => {
 const BigAssWheel = ({
     data, ...props
 }) => {
+    const [restaurant, setRestaurant] = useState("");
 
-    const padding = {top:20, right:40, bottom:0, left:0},
+    const padding = {top:0, right:40, bottom:0, left:0},
         w = 500 - padding.left - padding.right,
         h = 500 - padding.top  - padding.bottom,
         r = Math.min(w, h)/2,
@@ -41,39 +44,6 @@ const BigAssWheel = ({
     function textColor(d) {
         return d3.hsl(color(d)).l > 0.5 ? "#000" : "#fff";
     }
-
-    // const bigData = [
-    //     {year: 1980, efficiency: 24.3, sales: 8949000},
-    //     {year: 1985, efficiency: 27.6, sales: 10979000},
-    //     {year: 1990, efficiency: 28, sales: 9303000},
-    //     {year: 1991, efficiency: 28.4, sales: 8185000},
-    //     {year: 1992, efficiency: 27.9, sales: 8213000},
-    //     {year: 1993, efficiency: 28.4, sales: 8518000},
-    //     {year: 1994, efficiency: 28.3, sales: 8991000},
-    //     {year: 1995, efficiency: 28.6, sales: 8620000},
-    //     {year: 1996, efficiency: 28.5, sales: 8479000},
-    //     {year: 1997, efficiency: 28.7, sales: 8217000},
-    //     {year: 1998, efficiency: 28.8, sales: 8085000},
-    //     {year: 1999, efficiency: 28.3, sales: 8638000},
-    //     {year: 2000, efficiency: 28.5, sales: 8778000},
-    //     {year: 2001, efficiency: 28.8, sales: 8352000},
-    //     {year: 2002, efficiency: 29, sales: 8042000},
-    //     {year: 2003, efficiency: 29.5, sales: 7556000},
-    //     {year: 2004, efficiency: 29.5, sales: 7483000},
-    //     {year: 2005, efficiency: 30.3, sales: 7660000},
-    //     {year: 2006, efficiency: 30.1, sales: 7762000},
-    //     {year: 2007, efficiency: 31.2, sales: 7562000},
-    //     {year: 2008, efficiency: 31.5, sales: 6769000},
-    //     {year: 2009, efficiency: 32.9, sales: 5402000},
-    //     {year: 2010, efficiency: 33.9, sales: 5636000},
-    //     {year: 2011, efficiency: 33.1, sales: 6093000},
-    //     {year: 2012, efficiency: 35.3, sales: 7245000},
-    //     {year: 2013, efficiency: 36.4, sales: 7586000},
-    //     {year: 2014, efficiency: 36.5, sales: 7708000},
-    //     {year: 2015, efficiency: 37.2, sales: 7517000},
-    //     {year: 2016, efficiency: 37.7, sales: 6873000},
-    //     {year: 2017, efficiency: 39.4, sales: 6081000},
-    //   ];
 
     const ref = useD3(
       (svg) => {
@@ -210,7 +180,6 @@ const BigAssWheel = ({
               console.log(props.spinning);
             }
             //all slices have been seen, all done
-            console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
             if(oldpick.length == data.length){
                 console.log("done");
                 container.on("click", null);
@@ -230,7 +199,6 @@ const BigAssWheel = ({
             // } else {
             //     oldpick.push(picked);
             // }
-          console.log(picked);
             rotation += 90 - Math.round(ps/2);
             vis.transition()
                 .duration(3000)
@@ -250,6 +218,7 @@ const BigAssWheel = ({
                     /* Comment the below line for restrict spin to sngle time */
                     // container.on("click", spin);
                     props.spinning = false;
+                    setRestaurant(data[picked].label);
                 });
         }
         //make arrow
@@ -300,19 +269,22 @@ const BigAssWheel = ({
     );
   
     return (
-      <svg
-        ref={ref}
-        style={{
-          height: h+"px",
-          width: w+"px",
-          marginRight: "0px",
-          marginLeft: "0px",
-        }}
-      >
-        <g className="plot-area" />
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
+      <>
+        <svg
+          ref={ref}
+          style={{
+            height: h+"px",
+            width: w+"px",
+            marginRight: "0px",
+            marginLeft: "0px",
+          }}
+        >
+          <g className="plot-area" />
+          <g className="x-axis" />
+          <g className="y-axis" />
+        </svg>
+        <RestaurantCard name={restaurant}/>
+      </>
     );
 };
 
